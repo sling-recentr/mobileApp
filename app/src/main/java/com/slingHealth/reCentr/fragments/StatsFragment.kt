2f748860391dc865013653b211dfb3a27fd5153d
Robert Goodloe <rjgoodloe@gmail.com>
2019-02-02 15:54:45 -0600

    pre-bluetooth

--- app/src/main/java/com/slingHealth/reCentr/fragments/StatsFragment.kt
+++ app/src/main/java/com/slingHealth/reCentr/fragments/StatsFragment.kt
@@ -17,10 +17,10 @@ class StatsFragment(context: Context) : Fragment() {
     private var parentContext: Context = context
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view: View = inflater.inflate(R.layout.home_fragment, container, false)
+        val view: View = inflater.inflate(R.layout.stats_fragment, container, false)
 
 
-        return inflater.inflate(R.layout.home_fragment, container, false)
+        return view
     }
 
     override fun onStart() {
